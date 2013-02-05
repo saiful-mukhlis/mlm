291c0e74441a40db2d4c235add1c7774de4dfb0a
Saiful Mukhlis <saiful.mukhlis@gmail.com>
2013-02-05 09:16:27 +0700

    del
    delete window interface dan window abstract

--- src/org/basic/comp/abst/WindowAbstract.java
+++ src/org/basic/comp/abst/WindowAbstract.java
@@ -1,224 +1,224 @@
-package org.basic.comp.abst;
-
-import com.basic.lang.LApp;
-import com.basic.lang.LWindow;
-import com.global.App;
-import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
-import org.basic.comp.adapter.MenuAdapter;
-import org.basic.comp.adapter.ToolbarAdapter;
-import org.basic.comp.adapter.WindowAdapter;
-import org.basic.comp.base.BosOne;
-import org.basic.comp.base.PanelBottom;
-import org.noos.xing.mydoggy.Content;
-import org.noos.xing.mydoggy.ContentManagerListener;
-import org.noos.xing.mydoggy.ToolWindow;
-import org.noos.xing.mydoggy.ToolWindowManager;
-import org.noos.xing.mydoggy.event.ContentManagerEvent;
-import org.noos.xing.mydoggy.mydoggyset.action.AddContentAction;
-import org.noos.xing.mydoggy.plaf.MyDoggyToolWindowManager;
-import org.noos.xing.mydoggy.plaf.ui.cmp.ExtendedTableLayout;
-import org.noos.xing.yasaf.view.ViewContext;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public abstract class WindowAbstract extends WindowAdapter{
-	protected ToolbarAdapter toolbar;
-	protected MenuAdapter menu;
-	protected ToolWindowManager toolWindowManager;
-	protected ViewContext myDoggySetContext;
-	protected JFrame frame;
-	protected PanelBottom panelBottom;
-	
-	protected JProgressBar progressBar;
-	protected boolean useBar=false;
-	protected BosOne bosOne;
-	
-	protected List<WidgetPrivilege> cangeHakAkses = new ArrayList<WidgetPrivilege>();
-	
-	protected String idSelected="";
-	
-	@Override
-	public void build(ODatabaseDocumentTx db) {
-		init(db);
-		
-	}
-	public void init(ODatabaseDocumentTx db){
-		setValueBar(75);
-		seta(db);
-		setValueBar(80);
-		initFrame(db);
-		initToolWindowManager();
-		buildMaster(db);
-		initContext(db);
-		buildActions(db);
-		initMenu(db);
-		initToolbar(db);
-		
-		setLocationFrame();
-		setAvailableWindow();
-		setValueBar(90);
-		manageWelcome();
-		showWelcome();
-		setValueBar(99);
-		showFrame();
-	}
-	public void manageWelcome() {
-		toolWindowManager.getContentManager().addContentManagerListener(
-				new ContentManagerListener() {
-
-					@Override
-					public void contentSelected(ContentManagerEvent arg0) {
-						// TODO Auto-generated method stub
-						idSelected=arg0.getContent().getId();
-						if (getKomponentSeledcted()!=null) {
-							getKomponentSeledcted().getWidgetTop().requestDefaultSelected();
-						}
-						
-					}
-
-					@Override
-					public void contentRemoved(ContentManagerEvent arg0) {
-						// TODO Auto-generated method stub
-						if (arg0.getContent().getId().equals(idSelected)) {
-							idSelected="";
-						}
-						if (toolWindowManager.getContentManager()
-								.getContentCount() == 0) {
-							showWelcome();
-						}
-					}
-
-					@Override
-					public void contentAdded(ContentManagerEvent arg0) {
-						// TODO Auto-generated method stub
-						idSelected=arg0.getContent().getId();
-						if (toolWindowManager.getContentManager()
-								.getContentCount() > 1) {
-							try {
-								Content c = toolWindowManager.getContentManager()
-								.getContentByComponent(getComponentWelcome());
-						toolWindowManager.getContentManager()
-								.removeContent(c);
-							} catch (Exception e) {
-								// TODO: handle exception
-							}
-							
-						}
-						
-					}
-				});
-	}
-	public void setAvailableWindow() {
-		for (ToolWindow window : toolWindowManager.getToolWindows()) {
-			window.setAvailable(true);
-		}
-	}
-	public void initToolWindowManager() {
-		JPanel p = new JPanel();
-		p.setLayout(new ExtendedTableLayout(new double[][] { { 0, -1, 0 },
-				{ 0, -1, 0 } }));
-
-		final MyDoggyToolWindowManager myDoggyToolWindowManager = new MyDoggyToolWindowManager();
-		toolWindowManager = myDoggyToolWindowManager;
-		
-		p.add(myDoggyToolWindowManager, "1,1,");
-		frame.add(p, BorderLayout.CENTER);
-
-	}
-	
-	
-	public void setValueBar(int value){
-		if (useBar) {
-			progressBar.setValue(value);
-		}
-	}
-	
-	public void initFrame(ODatabaseDocumentTx db) {
-//		frame = new JFrame(LWindow.title+" "+bosOne.getLoc());
-		frame = new JFrame(LWindow.TITLE);
-		frame.setExtendedState( frame.getExtendedState()|JFrame.MAXIMIZED_BOTH );
-		frame.setIconImage(App.getImage(LApp.iconApp16).getImage());
-		frame.getContentPane().setLayout(new BorderLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		panelBottom=new PanelBottom();
-		panelBottom.build();
-		frame.getContentPane().add(panelBottom.getPanel(), BorderLayout.SOUTH);
-	}
-	
-	public void setLocationFrame() {
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Dimension screenSize = toolkit.getScreenSize();
-		int screenResolution = toolkit.getScreenResolution();
-		float zoom = screenResolution / 72.0F;
-
-		int height = (int) (550.0F * zoom);
-		if (height > screenSize.getHeight()) {
-			height = (int) screenSize.getHeight();
-		}
-		int width = (int) (750.0F * zoom);
-		if (width > screenSize.getWidth()) {
-			width = (int) screenSize.getWidth();
-		}
-
-		Dimension dimension = new Dimension(width, height);
-		frame.setSize(dimension);
-		frame.setLocation((screenSize.width - width) / 2,
-				(screenSize.height - height) / 2);
-	}
-	public void showFrame() {
-		frame.setVisible(true);
-	}
-	
-	public AddContentAction factoryContentAction(ODatabaseDocumentTx db, String title, Icon icon,
-			Component component, int shortCut) {
-		return new AddContentAction(toolWindowManager,
-				title, title,
-				icon, component, title, shortCut);
-	}
-	public ToolbarAdapter getToolbar() {
-		return toolbar;
-	}
-	public void setToolbar(ToolbarAdapter toolbar) {
-		this.toolbar = toolbar;
-	}
-	public void setProgressBar(JProgressBar progressBar) {
-		this.progressBar = progressBar;
-	}
-	public ToolWindowManager getToolWindowManager() {
-		return toolWindowManager;
-	}
-	public void setToolWindowManager(ToolWindowManager toolWindowManager) {
-		this.toolWindowManager = toolWindowManager;
-	}
-	public String getIdSelected() {
-		return idSelected;
-	}
-	public void setIdSelected(String idSelected) {
-		this.idSelected = idSelected;
-	}
-	public MenuAdapter getMenu() {
-		return menu;
-	}
-	public void setMenu(MenuAdapter menu) {
-		this.menu = menu;
-	}
-	public PanelBottom getPanelBottom() {
-		return panelBottom;
-	}
-	public void setPanelBottom(PanelBottom panelBottom) {
-		this.panelBottom = panelBottom;
-	}
-	public JFrame getFrame() {
-		return frame;
-	}
-	public void setFrame(JFrame frame) {
-		this.frame = frame;
-	}
-	
-	
-	
-}
+//package org.basic.comp.abst;
+//
+//import com.basic.lang.LApp;
+//import com.basic.lang.LWindow;
+//import com.global.App;
+//import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+//import org.basic.comp.adapter.MenuAdapter;
+//import org.basic.comp.adapter.ToolbarAdapter;
+//import org.basic.comp.adapter.WindowAdapter;
+//import org.basic.comp.base.BosOne;
+//import org.basic.comp.base.PanelBottom;
+//import org.noos.xing.mydoggy.Content;
+//import org.noos.xing.mydoggy.ContentManagerListener;
+//import org.noos.xing.mydoggy.ToolWindow;
+//import org.noos.xing.mydoggy.ToolWindowManager;
+//import org.noos.xing.mydoggy.event.ContentManagerEvent;
+//import org.noos.xing.mydoggy.mydoggyset.action.AddContentAction;
+//import org.noos.xing.mydoggy.plaf.MyDoggyToolWindowManager;
+//import org.noos.xing.mydoggy.plaf.ui.cmp.ExtendedTableLayout;
+//import org.noos.xing.yasaf.view.ViewContext;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public abstract class WindowAbstract extends WindowAdapter{
+//	protected ToolbarAdapter toolbar;
+//	protected MenuAdapter menu;
+//	protected ToolWindowManager toolWindowManager;
+//	protected ViewContext myDoggySetContext;
+//	protected JFrame frame;
+//	protected PanelBottom panelBottom;
+//	
+//	protected JProgressBar progressBar;
+//	protected boolean useBar=false;
+//	protected BosOne bosOne;
+//	
+//	protected List<WidgetPrivilege> cangeHakAkses = new ArrayList<WidgetPrivilege>();
+//	
+//	protected String idSelected="";
+//	
+//	@Override
+//	public void build(ODatabaseDocumentTx db) {
+//		init(db);
+//		
+//	}
+//	public void init(ODatabaseDocumentTx db){
+//		setValueBar(75);
+//		seta(db);
+//		setValueBar(80);
+//		initFrame(db);
+//		initToolWindowManager();
+//		buildMaster(db);
+//		initContext(db);
+//		buildActions(db);
+//		initMenu(db);
+//		initToolbar(db);
+//		
+//		setLocationFrame();
+//		setAvailableWindow();
+//		setValueBar(90);
+//		manageWelcome();
+//		showWelcome();
+//		setValueBar(99);
+//		showFrame();
+//	}
+//	public void manageWelcome() {
+//		toolWindowManager.getContentManager().addContentManagerListener(
+//				new ContentManagerListener() {
+//
+//					@Override
+//					public void contentSelected(ContentManagerEvent arg0) {
+//						// TODO Auto-generated method stub
+//						idSelected=arg0.getContent().getId();
+//						if (getKomponentSeledcted()!=null) {
+//							getKomponentSeledcted().getWidgetTop().requestDefaultSelected();
+//						}
+//						
+//					}
+//
+//					@Override
+//					public void contentRemoved(ContentManagerEvent arg0) {
+//						// TODO Auto-generated method stub
+//						if (arg0.getContent().getId().equals(idSelected)) {
+//							idSelected="";
+//						}
+//						if (toolWindowManager.getContentManager()
+//								.getContentCount() == 0) {
+//							showWelcome();
+//						}
+//					}
+//
+//					@Override
+//					public void contentAdded(ContentManagerEvent arg0) {
+//						// TODO Auto-generated method stub
+//						idSelected=arg0.getContent().getId();
+//						if (toolWindowManager.getContentManager()
+//								.getContentCount() > 1) {
+//							try {
+//								Content c = toolWindowManager.getContentManager()
+//								.getContentByComponent(getComponentWelcome());
+//						toolWindowManager.getContentManager()
+//								.removeContent(c);
+//							} catch (Exception e) {
+//								// TODO: handle exception
+//							}
+//							
+//						}
+//						
+//					}
+//				});
+//	}
+//	public void setAvailableWindow() {
+//		for (ToolWindow window : toolWindowManager.getToolWindows()) {
+//			window.setAvailable(true);
+//		}
+//	}
+//	public void initToolWindowManager() {
+//		JPanel p = new JPanel();
+//		p.setLayout(new ExtendedTableLayout(new double[][] { { 0, -1, 0 },
+//				{ 0, -1, 0 } }));
+//
+//		final MyDoggyToolWindowManager myDoggyToolWindowManager = new MyDoggyToolWindowManager();
+//		toolWindowManager = myDoggyToolWindowManager;
+//		
+//		p.add(myDoggyToolWindowManager, "1,1,");
+//		frame.add(p, BorderLayout.CENTER);
+//
+//	}
+//	
+//	
+//	public void setValueBar(int value){
+//		if (useBar) {
+//			progressBar.setValue(value);
+//		}
+//	}
+//	
+//	public void initFrame(ODatabaseDocumentTx db) {
+////		frame = new JFrame(LWindow.title+" "+bosOne.getLoc());
+//		frame = new JFrame(LWindow.TITLE);
+//		frame.setExtendedState( frame.getExtendedState()|JFrame.MAXIMIZED_BOTH );
+//		frame.setIconImage(App.getImage(LApp.iconApp16).getImage());
+//		frame.getContentPane().setLayout(new BorderLayout());
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		
+//		panelBottom=new PanelBottom();
+//		panelBottom.build();
+//		frame.getContentPane().add(panelBottom.getPanel(), BorderLayout.SOUTH);
+//	}
+//	
+//	public void setLocationFrame() {
+//		Toolkit toolkit = Toolkit.getDefaultToolkit();
+//		Dimension screenSize = toolkit.getScreenSize();
+//		int screenResolution = toolkit.getScreenResolution();
+//		float zoom = screenResolution / 72.0F;
+//
+//		int height = (int) (550.0F * zoom);
+//		if (height > screenSize.getHeight()) {
+//			height = (int) screenSize.getHeight();
+//		}
+//		int width = (int) (750.0F * zoom);
+//		if (width > screenSize.getWidth()) {
+//			width = (int) screenSize.getWidth();
+//		}
+//
+//		Dimension dimension = new Dimension(width, height);
+//		frame.setSize(dimension);
+//		frame.setLocation((screenSize.width - width) / 2,
+//				(screenSize.height - height) / 2);
+//	}
+//	public void showFrame() {
+//		frame.setVisible(true);
+//	}
+//	
+//	public AddContentAction factoryContentAction(ODatabaseDocumentTx db, String title, Icon icon,
+//			Component component, int shortCut) {
+//		return new AddContentAction(toolWindowManager,
+//				title, title,
+//				icon, component, title, shortCut);
+//	}
+//	public ToolbarAdapter getToolbar() {
+//		return toolbar;
+//	}
+//	public void setToolbar(ToolbarAdapter toolbar) {
+//		this.toolbar = toolbar;
+//	}
+//	public void setProgressBar(JProgressBar progressBar) {
+//		this.progressBar = progressBar;
+//	}
+//	public ToolWindowManager getToolWindowManager() {
+//		return toolWindowManager;
+//	}
+//	public void setToolWindowManager(ToolWindowManager toolWindowManager) {
+//		this.toolWindowManager = toolWindowManager;
+//	}
+//	public String getIdSelected() {
+//		return idSelected;
+//	}
+//	public void setIdSelected(String idSelected) {
+//		this.idSelected = idSelected;
+//	}
+//	public MenuAdapter getMenu() {
+//		return menu;
+//	}
+//	public void setMenu(MenuAdapter menu) {
+//		this.menu = menu;
+//	}
+//	public PanelBottom getPanelBottom() {
+//		return panelBottom;
+//	}
+//	public void setPanelBottom(PanelBottom panelBottom) {
+//		this.panelBottom = panelBottom;
+//	}
+//	public JFrame getFrame() {
+//		return frame;
+//	}
+//	public void setFrame(JFrame frame) {
+//		this.frame = frame;
+//	}
+//	
+//	
+//	
+//}
--- src/org/basic/comp/adapter/WindowInterfaces.java
+++ src/org/basic/comp/adapter/WindowInterfaces.java
@@ -1,53 +1,53 @@
-package org.basic.comp.adapter;
-
-import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
-import org.basic.comp.base.Komponent;
-import org.basic.comp.base.PanelBottom;
-import org.basic.comp.listener.WidgetInterface;
-import org.noos.xing.mydoggy.ToolWindowManager;
-import org.noos.xing.mydoggy.mydoggyset.action.AddContentAction;
-
-import java.awt.*;
-import java.util.HashMap;
-import java.util.List;
-
-public interface WindowInterfaces extends WidgetInterface {
-public void initToolbar(ODatabaseDocumentTx db);
-public void initMenu(ODatabaseDocumentTx db);
-public MenuInterface getMenu();
-public void initContext(ODatabaseDocumentTx db);
-public void showWelcome();
-public Component getComponentWelcome();
-public void seta(ODatabaseDocumentTx db);
-public void buildMaster(ODatabaseDocumentTx db);
-public void buildActions(ODatabaseDocumentTx db);
-public ToolWindowManager getToolWindowManager();
-
-//public AddContentAction getUsrAca();
-public AddContentAction getWelcomeAca();
-//public AddContentAction getKandangAca();
-
-public List<Komponent> getKomponents();
-public HashMap<String, Komponent> getKomponentMaps();
-public Komponent getKomponentSeledcted();
-
-public ToolbarAdapter getToolbar();
-public PanelBottom getPanelBottom();
-public void login();
-
-public void actionClose();
-public void actionExit();
-public void actionPrint();
-public void actionAdd();
-public void actionEdit();
-public void actionDel();
-public void actionView();
-public void actionReload();
-
-public void actionReg();
-public void actionAbout();
-
-
-public void showToolbar();
-
-}
+//package org.basic.comp.adapter;
+//
+//import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+//import org.basic.comp.base.Komponent;
+//import org.basic.comp.base.PanelBottom;
+//import org.basic.comp.listener.WidgetInterface;
+//import org.noos.xing.mydoggy.ToolWindowManager;
+//import org.noos.xing.mydoggy.mydoggyset.action.AddContentAction;
+//
+//import java.awt.*;
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public interface WindowInterfaces extends WidgetInterface {
+//public void initToolbar(ODatabaseDocumentTx db);
+//public void initMenu(ODatabaseDocumentTx db);
+//public MenuInterface getMenu();
+//public void initContext(ODatabaseDocumentTx db);
+//public void showWelcome();
+//public Component getComponentWelcome();
+//public void seta(ODatabaseDocumentTx db);
+//public void buildMaster(ODatabaseDocumentTx db);
+//public void buildActions(ODatabaseDocumentTx db);
+//public ToolWindowManager getToolWindowManager();
+//
+////public AddContentAction getUsrAca();
+//public AddContentAction getWelcomeAca();
+////public AddContentAction getKandangAca();
+//
+//public List<Komponent> getKomponents();
+//public HashMap<String, Komponent> getKomponentMaps();
+//public Komponent getKomponentSeledcted();
+//
+//public ToolbarAdapter getToolbar();
+//public PanelBottom getPanelBottom();
+//public void login();
+//
+//public void actionClose();
+//public void actionExit();
+//public void actionPrint();
+//public void actionAdd();
+//public void actionEdit();
+//public void actionDel();
+//public void actionView();
+//public void actionReload();
+//
+//public void actionReg();
+//public void actionAbout();
+//
+//
+//public void showToolbar();
+//
+//}
